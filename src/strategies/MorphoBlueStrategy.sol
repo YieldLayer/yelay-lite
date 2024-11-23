928e42e2531b5c0bbe2e7367c3375f08b678ae53
ksamarin90 <ksamarin90@gmail.com>
2024-11-23 16:45:57 +0400

    morpho blue strategy

---
+++ src/strategies/MorphoBlueStrategy.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import {MorphoBalancesLib} from "@morpho-blue/libraries/periphery/MorphoBalancesLib.sol";
+import {Id, IMorpho, MarketParams} from "@morpho-blue/interfaces/IMorpho.sol";
+
+import {IStrategyBase} from "src/interfaces/IStrategyBase.sol";
+import {IERC20} from "@openzeppelin/token/ERC20/IERC20.sol";
+
+contract MorphoBlueStrategy is IStrategyBase {
+    IMorpho immutable morpho;
+
+    constructor(address morpho_) {
+        morpho = IMorpho(morpho_);
+    }
+
+    function _decodeSupplement(bytes calldata supplement) internal pure returns (address asset, Id id) {
+        return abi.decode(supplement, (address, Id));
+    }
+
+    function protocol() external view returns (address) {
+        return address(morpho);
+    }
+
+    function deposit(uint256 amount, bytes calldata supplement) external {
+        (, Id id) = _decodeSupplement(supplement);
+        MarketParams memory marketParams = morpho.idToMarketParams(id);
+        morpho.supply(marketParams, amount, 0, address(this), "");
+    }
+
+    function withdraw(uint256 amount, bytes calldata supplement) external {
+        (, Id id) = _decodeSupplement(supplement);
+        MarketParams memory marketParams = morpho.idToMarketParams(id);
+        morpho.withdraw(marketParams, amount, 0, address(this), address(this));
+    }
+
+    function assetBalance(address yelayLiteVault, bytes calldata supplement) external view returns (uint256) {
+        (, Id id) = _decodeSupplement(supplement);
+        MarketParams memory marketParams = morpho.idToMarketParams(id);
+        return MorphoBalancesLib.expectedSupplyAssets(morpho, marketParams, yelayLiteVault);
+    }
+
+    function onAdd(bytes calldata supplement) external {
+        (address asset,) = _decodeSupplement(supplement);
+        IERC20(asset).approve(address(morpho), type(uint256).max);
+    }
+
+    function onRemove(bytes calldata supplement) external {
+        (address asset,) = _decodeSupplement(supplement);
+        IERC20(asset).approve(address(morpho), 0);
+    }
+
+    // function claimRewards() external {}
+    // function viewRewards() external returns (address[] memory tokens, uint256[] memory amounts) {}
+}
