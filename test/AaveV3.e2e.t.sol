384b2ec493c85b7cff881ba0b4d2034accfe847a
ksamarin90 <ksamarin90@gmail.com>
2024-11-23 14:14:54 +0400

    generic strategy test

---
+++ test/AaveV3.e2e.t.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {ManagementFacet} from "src/facets/ManagementFacet.sol";
+import {LibManagement} from "src/libraries/LibManagement.sol";
+
+import {AbstractStrategyTest} from "./AbstractStrategyTest.sol";
+
+import {IPool} from "src/interfaces/external/aave/v3/IPool.sol";
+import {AaveV3Strategy} from "src/strategies/AaveV3Strategy.sol";
+import {AAVE_V3_POOL} from "./Constants.sol";
+
+contract AaveV3Test is AbstractStrategyTest {
+    function _setupStrategy() internal override {
+        vm.startPrank(owner);
+        IPool.ReserveData memory reserveData = IPool(AAVE_V3_POOL).getReserveData(address(underlyingAsset));
+        strategyShare = reserveData.aTokenAddress;
+        strategyAdapter = address(new AaveV3Strategy(AAVE_V3_POOL, address(underlyingAsset), strategyShare));
+        LibManagement.StrategyData memory strategy =
+            LibManagement.StrategyData({adapter: strategyAdapter, supplement: ""});
+        ManagementFacet(yelayLiteVault).addStrategy(strategy);
+        uint256[] memory queue = new uint256[](1);
+        queue[0] = 0;
+        ManagementFacet(yelayLiteVault).updateDepositQueue(queue);
+        ManagementFacet(yelayLiteVault).updateWithdrawQueue(queue);
+        vm.stopPrank();
+    }
+}
