8905f81a156fe36a4e6c55ac6b2e98096592bb12
ksamarin90 <ksamarin90@gmail.com>
2025-02-27 15:47:05 +0400

    check setup both vaults

--- scripts/base-production/check-setup.ts
+++ scripts/base-production/check-setup.ts
@@ -19,22 +19,54 @@ import {
 } from '../utils';
 
 async function main() {
-    const asset = 'WETH';
-    const yelayLiteVault = IYelayLiteVault__factory.connect(
-        contracts.vaults[asset],
-        ethers.provider,
-    );
+    for (const asset of ['USDC', 'WETH'] as const) {
+        console.log(`Working on ${asset} vault....`);
+        console.log('');
+        const yelayLiteVault = IYelayLiteVault__factory.connect(
+            contracts.vaults[asset],
+            ethers.provider,
+        );
 
-    console.log('Checking selectors...');
-    await checkFacets(yelayLiteVault, contracts.accessFacet, getAccessFacetSelectors());
-    await checkFacets(yelayLiteVault, contracts.ownerFacet, getOwnerFacetSelectors());
-    await checkFacets(yelayLiteVault, contracts.fundsFacet, getFundsFacetSelectors());
-    await checkFacets(yelayLiteVault, contracts.managementFacet, getManagementFacetSelectors());
-    await checkFacets(yelayLiteVault, contracts.clientsFacet, getClientFacetSelectors());
+        console.log('Checking selectors...');
+        await checkFacets(yelayLiteVault, contracts.accessFacet, getAccessFacetSelectors());
+        await checkFacets(yelayLiteVault, contracts.ownerFacet, getOwnerFacetSelectors());
+        await checkFacets(yelayLiteVault, contracts.fundsFacet, getFundsFacetSelectors());
+        await checkFacets(yelayLiteVault, contracts.managementFacet, getManagementFacetSelectors());
+        await checkFacets(yelayLiteVault, contracts.clientsFacet, getClientFacetSelectors());
 
+        console.log('');
+
+        console.log(`Getting addresses...`);
+        await yelayLiteVault.owner().then((r) => console.log(`Vault owner: ${r}`));
+        await yelayLiteVault.yieldExtractor().then((r) => console.log(`YieldExtractor: ${r}`));
+        await yelayLiteVault.underlyingAsset().then((r) => console.log(`Underlying asset: ${r}`));
+
+        console.log('');
+        console.log('Logging role members....');
+        console.log('');
+
+        await logRoleMembers(yelayLiteVault, 'STRATEGY_AUTHORITY');
+        console.log('');
+        await logRoleMembers(yelayLiteVault, 'QUEUES_OPERATOR');
+        console.log('');
+        await logRoleMembers(yelayLiteVault, 'FUNDS_OPERATOR');
+        console.log('');
+        await logRoleMembers(yelayLiteVault, 'SWAP_REWARDS_OPERATOR');
+        console.log('');
+        await logRoleMembers(yelayLiteVault, 'PAUSER');
+        console.log('');
+        await logRoleMembers(yelayLiteVault, 'UNPAUSER');
+
+        console.log('');
+        console.log('Checking swapper on vault....');
+        console.log('');
+
+        await checkSwapper(yelayLiteVault, contracts.swapper.proxy);
+        console.log('');
+    }
+
+    console.log(`Working on swapper and vaultWrapper...`);
     console.log('');
-
-    console.log(`Getting addresses...`);
 
     await Swapper__factory.connect(contracts.swapper.proxy, ethers.provider)
         .owner()
@@ -42,26 +74,6 @@ async function main() {
     await VaultWrapper__factory.connect(contracts.vaultWrapper.proxy, ethers.provider)
         .owner()
         .then((r) => console.log(`VaultWrapper owner: ${r}`));
-    await yelayLiteVault.owner().then((r) => console.log(`Vault owner: ${r}`));
-    await yelayLiteVault.owner().then((r) => console.log(`Vault owner: ${r}`));
-    await yelayLiteVault.yieldExtractor().then((r) => console.log(`YieldExtractor: ${r}`));
-    await yelayLiteVault.underlyingAsset().then((r) => console.log(`Underlying asset: ${r}`));
-
-    console.log('');
-    console.log('Logging role members....');
-    console.log('');
-
-    await logRoleMembers(yelayLiteVault, 'STRATEGY_AUTHORITY');
-    console.log('');
-    await logRoleMembers(yelayLiteVault, 'QUEUES_OPERATOR');
-    console.log('');
-    await logRoleMembers(yelayLiteVault, 'FUNDS_OPERATOR');
-    console.log('');
-    await logRoleMembers(yelayLiteVault, 'SWAP_REWARDS_OPERATOR');
-    console.log('');
-    await logRoleMembers(yelayLiteVault, 'PAUSER');
-    console.log('');
-    await logRoleMembers(yelayLiteVault, 'UNPAUSER');
 
     console.log('');
     console.log('Checking swapper implementation....');
@@ -84,10 +96,9 @@ async function main() {
     );
 
     console.log('');
-    console.log('Checking swapper on vault and vaultWrapper....');
+    console.log('Checking swapper on vaultWrapper....');
     console.log('');
 
-    await checkSwapper(yelayLiteVault, contracts.swapper.proxy);
     await checkSwapper(
         VaultWrapper__factory.connect(contracts.vaultWrapper.proxy, ethers.provider),
         contracts.swapper.proxy,
