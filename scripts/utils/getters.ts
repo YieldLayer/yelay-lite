2a56c8f9b2ffb3b0e45e9a142c312e32089aff4f
Michal Orlicek <mo@faculty.group>
2026-03-21 01:10:36 +0100

    Update status script

--- scripts/utils/getters.ts
+++ scripts/utils/getters.ts
@@ -38,6 +38,7 @@ export const getFundsFacetSelectors = () => {
         'strategyRewards',
         'deposit',
         'redeem',
+        'claimAndRedeem',
         'migratePosition',
         'managedDeposit',
         'managedWithdraw',
